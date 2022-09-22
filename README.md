4a3e3859799f586503a80aa15edcab689c230763
Sergio Gonzalez <sergiogonzalezlopez101@gmail.com>
2022-09-21 23:46:50 -0500

    Added new paths to access pokedex.json items (All, by id and by name)

---
+++ README.md
@@ -0,0 +1,1 @@
+# Development of an API with NodeJS
--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 //console.log("Hola mundo");
+const { response } = require('express');
 const express = require('express');
 const app = express();
+const { pokemon } = require('./pokedex.json');
 
 app.get('/', (req, res, next) => {
-    res.send('Bienvenido <br> Hello World! ');
+    res.status(200);
+    res.send("Bienvenido al pokedex");
 });
 
-app.listen(3000, () => {
-    console.log('Server is running');
+app.get('/pokemon/all', (req, res, next) => {
+    res.status(200);
+    res.send(pokemon);
+});
+
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
+    const id = req.params.id - 1;
+    if(id >=0 && id <= 150) {
+        res.status(200);
+        return res.send(pokemon[req.params.id - 1]);
+    }
+    res.status(404);
+    res.send("Pokemon not found");
+
+});
+
+app.get('/pokemon/:name', (req, res, next) => {
+    const name = req.params.name;
+    for (const iterator in pokemon) {
+        if (pokemon[iterator].name === name) {
+            res.status(200);
+            res.send(pokemon[iterator]);
+        }
+    }
+    res.status(404);
+    res.send('Pokemon not Found');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running...');
 });
