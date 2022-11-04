cc604daff6ee91e85dd972b0be128a4a1507b789
Sergio Gonzalez <sergiogonzalezlopez101@gmail.com>
2022-11-04 17:34:39 -0600

    We return to json and in the index we added a middleware for url error

--- index.js
+++ index.js
@@ -9,10 +9,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res, next) => {
-    return res.status(200).send("Welcome to the pokedex");
+    return res.status(200).json({ code: 1, message: "Welcome to the pokedex"});
 });
 
 app.use("/pokemon", pokemon);
+
+app.use((req, res, next) => {
+    return res.status(404).json({ code: 404, message : "URL not found"})
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...');
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -8,23 +8,25 @@ pokemon.post('/', (req, res, next) => {
 
 pokemon.get('/', async (req, res, next) => {
     const pkmn = await db.query(`SELECT * FROM pokemon;`);
-    return res.status(200).json(pkmn);
+    return res.status(200).json({ code: 1, message: pkmn});
 });
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
     const id = req.params.id;
-    const pkmn = await db.query(`SELECT * FROM pokemon WHERE pok_id = ${id};`);
-    (id >=0 && id <= 25) ?
-        res.status(200).json(pkmn) :
-        res.status(404).send("Pokemon not found");
+    if (id >= 1 && id <= 722) {
+        const pkmn = await db.query(`SELECT * FROM pokemon WHERE pok_id = ${id};`);
+        return res.status(200).json({code: 1, message: pkmn});
+    }
+    return res.status(404).json({code: 404, message: "Pokemon not found"});
 });
 
 pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     const name = req.params.name.toLowerCase();
     const pkmn = await db.query(`SELECT * FROM pokemon WHERE pok_name = '${name}';`);
-    (pkmn.length > 0) ? 
-        res.status(200).json(pkmn) : 
-        res.status(404).send('Pokemon not Found');
+    if (pkmn.length > 0) {
+        return res.status(200).json({ code: 1, message: pkmn})
+    }
+    return res.status(404).json({ code: 404, message: 'Pokemon not Found'});
 });
 
 module.exports = pokemon;
