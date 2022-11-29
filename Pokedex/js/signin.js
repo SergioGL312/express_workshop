7e4fbb8cf25ceda5c6e51791176fc88af797a599
Sergio Gonzalez <sergiogonzalezlopez101@gmail.com>
2022-11-28 18:35:38 -0600

    Login and signin operation

---
+++ Pokedex/js/signin.js
@@ -0,0 +1,31 @@
+window.onload = init;
+
+function init() {
+    document.querySelector('.btn-secondary').addEventListener('click', function () {
+        window.location.href = "login.html"
+    });
+
+    document.querySelector('.btn-primary').addEventListener('click', signin);
+}
+
+function signin() {
+    var name = document.getElementById('input-name').value;
+    var mail = document.getElementById('input-mail').value;
+    var password = document.getElementById('input-password').value;
+
+    axios({
+        method: 'post',
+        url: 'http://localhost:3000/user/signin',
+        data: {
+            user_name: name,
+            user_mail: mail,
+            user_password: password
+        }
+    }).then(function(res){
+        console.log(res);
+        alert('User inserted correctly');
+        window.location.href = "login.html";
+    }).catch(function(err){
+        console.log(err);
+    });
+}
